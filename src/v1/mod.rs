408e64ab55cee94f1cc9153f1688ce84eaddc1f8
Pedro de Matos Fedricci <pedromfedricci@gmail.com>
2022-01-18 03:35:35 -0300

    Update static urls

--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -1,5 +1,4 @@
 #[cfg(any(feature = "blocking", feature = "async"))]
-#[macro_use]
 mod static_url;
 
 pub mod definition;
--- src/v1/static_url.rs
+++ src/v1/static_url.rs
@@ -34,6 +34,12 @@ macro_rules! paper_citations_endpoint {
     };
 }
 
+macro_rules! author_search_endpoint {
+    () => {
+        concat!(api_base_url_v1!(), "author/search")
+    };
+}
+
 macro_rules! author_endpoint {
     () => {
         concat!(api_base_url_v1!(), "author/{author_id}")
@@ -44,4 +50,36 @@ macro_rules! author_papers_endpoint {
     () => {
         concat!(author_endpoint!(), "/papers")
     };
+}
+
+pub(super) fn paper_search_endpoint() -> String {
+    paper_search_endpoint!().to_string()
+}
+
+pub(super) fn paper_endpoint(paper_id: &str) -> String {
+    format!(paper_endpoint!(), paper_id = paper_id)
+}
+
+pub(super) fn paper_authors_endpoint(paper_id: &str) -> String {
+    format!(paper_authors_endpoint!(), paper_id = paper_id)
+}
+
+pub(super) fn paper_references_endpoint(paper_id: &str) -> String {
+    format!(paper_references_endpoint!(), paper_id = paper_id)
+}
+
+pub(super) fn paper_citations_endpoint(paper_id: &str) -> String {
+    format!(paper_citations_endpoint!(), paper_id = paper_id)
+}
+
+pub(super) fn author_search_endpoint() -> String {
+    author_search_endpoint!().to_string()
+}
+
+pub(super) fn author_endpoint(author_id: &str) -> String {
+    format!(author_endpoint!(), author_id = author_id)
+}
+
+pub(super) fn author_papers_endpoint(author_id: &str) -> String {
+    format!(author_papers_endpoint!(), author_id = author_id)
 }
